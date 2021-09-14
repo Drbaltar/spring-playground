a498a267789a51c44f939c7bb59027736889ca60
Kyle McCain <kyle.t.mccain.mil@swf.army.mil>
2021-09-14 16:21:41 -0500

    Implemented solutions to checkpoints

--- src/main/java/com/drbaltar/springplayground/HelloController.java
+++ src/main/java/com/drbaltar/springplayground/HelloController.java
@@ -1,8 +1,9 @@
 package com.drbaltar.springplayground;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class HelloController {
@@ -15,5 +16,58 @@ public class HelloController {
     @GetMapping("/math/pi")
     public String getPi() {
         return String.valueOf(Math.PI);
+    }
+
+    @PostMapping("/comments")
+    public String getComments(@RequestParam String name, @RequestParam String city) {
+        return name + " lives in " + city + "!";
+    }
+
+    @GetMapping("/math/calculate")
+    public String getMathString(@RequestParam(required = false, defaultValue = "add") String operation, @RequestParam int x, @RequestParam int y) {
+        return MathService.getCalculate(operation, x, y);
+    }
+
+    @PostMapping("/math/sum")
+    public String postMathSum(@RequestParam MultiValueMap<String, String> qString) {
+        List<String> myList = qString.get("n");
+        return MathService.addAll(myList);
+    }
+
+    @RequestMapping("/math/volume/{length}/{width}/{height}")
+    public String calcVolume(@PathVariable int length, @PathVariable int width, @PathVariable int height) {
+        return String.format("The volume of a %sx%sx%s rectangle is %s", length, width, height, length * width * height);
+    }
+
+    @PostMapping(value = "/math/area", params = {"type", "radius"})
+    public String calcCircleArea(@RequestParam String type, @RequestParam String radius) {
+        int intRadius;
+        if (type.equals("circle")) {
+            try {
+                intRadius = Integer.parseInt(radius);
+            } catch (Exception e) {
+                return "Invalid";
+            }
+            return String.format("Area of a circle with a radius of %s is %.5f", radius, intRadius * intRadius * Math.PI);
+        } else {
+            return "Invalid";
+        }
+    }
+
+    @PostMapping(value = "/math/area", params = {"type", "width", "height"})
+    public String calcRectangleArea(@RequestParam String type, @RequestParam String width, @RequestParam String height) {
+        int intWidth;
+        int intHeight;
+        if (type.equals("rectangle")) {
+            try {
+                intWidth = Integer.parseInt(width);
+                intHeight = Integer.parseInt(height);
+            } catch (Exception e) {
+                return "Invalid";
+            }
+            return String.format("Area of a %dx%d rectangle is %d", intWidth, intHeight, intHeight * intWidth);
+        } else {
+            return "Invalid";
+        }
     }
 }
